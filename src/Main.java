0263ab58651ac937f95d4356528c1ce3624e57b1
fillnet <filipp@tenikaytis.ru>
2022-08-23 00:24:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public  Main() {
+            }
+    public static void main(String[] args){
+        byte a = 1;
+        short b = 2345;
+        int c = 23342342;
+        long d = 4535336474746L;
+        float e =1.343f;
+        double f = 1.456756456;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+}
